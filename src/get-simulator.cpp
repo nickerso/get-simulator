f65e57458a98b951b55bb02dbe53e1f239ea59f2
David Nickerson <david.nickerson@gmail.com>
2013-10-07 23:03:32 +1300

    add a brief description of what I'm trying to do, and convert some tabs to spaces

--- src/get-simulator.cpp
+++ src/get-simulator.cpp
@@ -17,9 +17,27 @@
   Can GET be a collection of code that gets combined with the generated code from CellML models and then compiled by LLVM at run time? or is GET an application that calls code generated from CellML models as required?
  */
 
+/*
+ * Given a "complete" CellML model describing the whole epithelial cell model configuration, we can
+ * create a GeneralModel with the required configuration by interrogating the CellML model directly.
+ *
+ * FIXME: eventually this would be done using the annotations, but for now assume predefined
+ *        component/variable names.
+ *
+ * Once we have the CellML model (code) we can put the required values into the places where they are needed
+ * and perform 'standard' model evaluations...in theory. We will also want to configure the simulation to be
+ * performed using SED-ML - but that is step 2.
+ */
+
 int main(int argc, char* argv[])
 {
-	// Main algorithm from Latta et al (1984), Figure 2.
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <CellML model>" << std::endl;
+        return -1;
+    }
+
+    // Main algorithm from Latta et al (1984), Figure 2.
     GeneralModel model;
 
     setDebugLevel(0);
@@ -36,59 +54,59 @@ int main(int argc, char* argv[])
     // we'll use CVODES for integration
     Cvodes cvodes;
 
-	/*
-	 * 1) transport parameters, initial conditions, t_initial, t_final, and
-	 * delta_t are specified, and integration variable t is initialised to t_initial.
-	 */
-	double t_initial = 0.0; // seconds
+    /*
+     * 1) transport parameters, initial conditions, t_initial, t_final, and
+     * delta_t are specified, and integration variable t is initialised to t_initial.
+     */
+    double t_initial = 0.0; // seconds
     double t_final = 1500; // seconds
     double delta_t = 1.0; // seconds
-	double t = t_initial;
-	model.initialise();
+    double t = t_initial;
+    model.initialise();
     model.printStateHeader(output);
     model.printState(output, t_initial);
     cvodes.initialise(&model, t_initial, delta_t);
 
     double initialVolume = model.V;
 
-	/*
+    /*
      * Solve to steady state in the open-circuit mode.
-	 */
+     */
     model.modelMode = GeneralModel::OpenCircuit;
     while (t <= t_final)
-	{
+    {
         double tout = t + delta_t;
-		/*
+        /*
          * 2) membrane potentials are computed.
          * @FIXME: this needs to be done inside the CVODE RHS function...but maybe ok if small enough time step?
-		 */
-//        model.mode = 1; // we want to solve for membrane potentials at the open-circuit conditions
-//        if (solveOneVariable(&model) > 0)
-//        {
-//            std::cerr << "Failed to solve open circuit case" << std::endl;
-//            break;
-//        }
+         */
+        //        model.mode = 1; // we want to solve for membrane potentials at the open-circuit conditions
+        //        if (solveOneVariable(&model) > 0)
+        //        {
+        //            std::cerr << "Failed to solve open circuit case" << std::endl;
+        //            break;
+        //        }
 
-		/*
-		 * 3) solute and water fluxes are computed using the membrane potentials and the
-		 * respective flux equations.
-		 */
-		/*
-		 * 4) the state equations are evaluated and are integrated to t+delta_t.
-		 */
+        /*
+         * 3) solute and water fluxes are computed using the membrane potentials and the
+         * respective flux equations.
+         */
+        /*
+         * 4) the state equations are evaluated and are integrated to t+delta_t.
+         */
         if (cvodes.integrate(t, tout) != 0)
         {
             std::cerr << "get: integration failed in steady-state block" << std::endl;
             output.close();
             return 1;
         }
-		/*
-		 * 5) additional variables are computed, and all results saved off-line.
-		 */
-		/*
-		 * 6) t is updated to t+delta_t and a comparison is made with t_final. If t_final
-		 * has been reached, the computation ends. Otherwise, the program returns to step
-		 * 2 to continue the integration.
+        /*
+         * 5) additional variables are computed, and all results saved off-line.
+         */
+        /*
+         * 6) t is updated to t+delta_t and a comparison is made with t_final. If t_final
+         * has been reached, the computation ends. Otherwise, the program returns to step
+         * 2 to continue the integration.
          */
 
         model.printState(output, t);
@@ -193,7 +211,7 @@ int main(int argc, char* argv[])
     }
 
     output.close();
-	return 0;
+    return 0;
 }
 
 
